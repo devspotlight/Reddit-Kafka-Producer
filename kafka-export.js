7e9dc11d0b2f4bb8958d69b2363c9f24b45b7afc
Jorge Orpinel <jorge@orpinel.com>
2019-05-07 02:19:10 -0400

    kafka-export: fix bug where `undefined` values were being sent to Kafka (thus lost); ...
    - Better stdout

--- kafka-export.js
+++ kafka-export.js
@@ -37,17 +37,18 @@ function fetchProfiles (cursor, n, queue) {
     n,
     /**
      * Anon callback to process all comments in these n rows from `profiles2`
-     * @param error
-     * @param rows
+     * @param err See https://node-postgres.com/api/cursor
+     * @param rows See https://node-postgres.com/api/cursor
      */
     (err, rows) => {
+      // NOTE: There should be `n` `rows` at this point.
       if (err) {
         console.error('kafka-export.js: fetchProfiles error!', err)
         throw err
       }
-      // console.debug('kafka-export.js: fetchProfiles next', n, 'rows of profiles:\n', rows.map(p => {
-      //   return { name: p.data.name, comments: p.data.comments.length }
-      // }))
+      console.info('kafka-export: next', n, 'rows of profiles:\n', rows.map(p => {
+        return { name: p.data.name, comments: p.data.comments.length }
+      }))
 
       // Processes each profile `row`.
       rows.forEach(
@@ -58,13 +59,13 @@ function fetchProfiles (cursor, n, queue) {
          * @returns {Promise<void>}
          */
         (profile) => {
-          console.debug('kafka-export.js: fetchProfiles x', n, 'processing profile', profile.data.name, 'with', profile.data.comments.length, 'comments')
+          console.info('kafka-export: processing profile', profile.data.name, 'with', profile.data.comments.length, 'comments')
           const comments = profile.data.comments
 
           let recentComments = [] // Start a data queue
 
           comments.forEach((comment) => {
-            const fullComment = formatComment(profile, comment)
+            const fullComment = formatComment(profile.data, comment)
 
             // Sets is_bot and is_troll (coming originally from bots.csv).
             fullComment.is_bot = profile.data.isBot
@@ -110,7 +111,7 @@ async function main () {
     // Connects to Potsgres using default env vars. See https://node-postgres.com/features/connecting
     const pool = new Pool({ ssl: true })
     const client = await pool.connect()
-    console.info('kafka-export.js: connected to Postgres at', process.env.PGHOST)
+    console.info('kafka-export: connected to Postgres at', process.env.PGHOST)
 
     // Creates Kafka producer. (Overwrites local files to use as Kafka credentials.)
     fs.writeFileSync('./client.crt', cert)
@@ -125,10 +126,10 @@ async function main () {
     })
 
     await producer.init()
-    console.info('kafka-export.js: connected to Kafka at', process.env.KAFKA_URL)
+    console.info('kafka-export: connected to Kafka at', process.env.KAFKA_URL)
 
     /* Db cursor to read from `profiles2` table */ // See https://node-postgres.com/api/cursor
-    console.info('kafka-export.js: querying', 'SELECT * FROM profiles2')
+    console.info('kafka-export: querying', 'SELECT * FROM profiles2')
     const cursor = client.query(new Cursor('SELECT * FROM profiles2'))
 
     /* Queue to insert comments into the db */
@@ -141,7 +142,7 @@ async function main () {
        * @returns {Promise<void>}
        */
       async (comment, cb) => {
-        // console.debug('kafka-export.js: kafkaQ sending Object.values of', comment.link_id, 'to Kafka')
+        // console.debug('kafka-export.js: kafkaQ sending JSON.stringify of', comment.link_id, 'to Kafka')
         wait(500)
         // Try sending stringified JSON `comment` to Kafka (async fn) after 500 ms.
         if (NODE_ENV === 'production') {
@@ -150,15 +151,16 @@ async function main () {
               topic: 'northcanadian-72923.reddit-comments',
               partition: 0,
               message: { value: JSON.stringify(comment) }
+              // TODO: Don't JSON.stringify `message`? See https://www.npmjs.com/package/kafka-node#sendpayloads-cb
             })
-            console.info('kafka-export.js: producer sent comment', comment.link_id, '- [0] offset', result[0].offset)
+            console.info('kafka-export: producer sent comment', comment.link_id, comment.created_utc, '- offset', result[0].offset)
             cb()
           } catch (error) {
             console.error('kafka-export.js: producer submission error!', error)
             cb()
           }
         } else {
-          console.debug('kafka-export.js: would produce/send JSON data for', comment.link_id, 'comment by', comment.author, '- Queue len', kafkaQ.length())
+          console.info('kafka-export: would produce/send JSON data for', comment.link_id, comment.created_utc, 'comment by', comment.author, '- Queue len', kafkaQ.length())
           cb()
         }
       }, 1)
@@ -173,10 +175,10 @@ async function main () {
       // TODO: What if the API is unavailable? `fetchProfiles` will keep running again and again but `kafkaQ` never grows?
       if (kafkaQ.length() > 99) {
         clearInterval(interval)
-        console.debug('kafka-export.js: suspending interval...')
+        // console.debug('kafka-export.js: suspending interval...')
         // Re-starts the interval when the last item from queue `kafkaQ` has returned from its worker.
         kafkaQ.drain = () => { // See https://caolan.github.io/async/docs.html#QueueObject
-          console.debug('kafka-export.js: restarting interval.')
+          // console.debug('kafka-export.js: restarting interval.')
           interval = setInterval(stream, 1000)
         }
       } else {
