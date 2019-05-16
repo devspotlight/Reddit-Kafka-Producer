devspotlight/Reddit-Kafka-Producer

commit 854cbfa872dcbe1469a8099ada5bea87b982316b

Changes isBot and isTroll for is_bot and is_troll to match universal schema.
(Properties of object returned by ProfileScraper.scrapeProfile)

kafka-export.js
@@ -70,8 +70,8 @@ function fetchProfiles (cursor, n, queue) {
             const fullComment = formatComment(profile.data, comment)
 
             // Sets is_bot and is_troll (coming originally from bots.csv).
-            fullComment.is_bot = profile.data.isBot
-            fullComment.is_troll = profile.data.isTroll
+            fullComment.is_bot = profile.data.is_bot
+            fullComment.is_troll = profile.data.is_troll
 
             // Attaches (≤20) previous comments by the same author to this comment.
             fullComment.recent_comments = JSON.stringify(recentComments.slice(1, 21)) // JSON formatted string

profile-scraper.js
@@ -76,7 +76,7 @@ ProfileScraper.prototype.fetchComments = async function (username, after) {
  * @param username to fetch profile and comments for
  * @param isBot is returned back as is in an object
  * @param isTroll is returned back as is in an object
- * @returns {Promise<*>} object with user profile, comments, `isBot`, and `isTroll
+ * @returns {Promise<*>} object with {<user profile data fields>, <comments>, `is_bot`, and `is_troll`}
  */
 ProfileScraper.prototype.scrapeProfile = async function (username, isBot, isTroll) {
   const user = await this.fetchProfile(username)
@@ -93,7 +93,7 @@ ProfileScraper.prototype.scrapeProfile = async function (username, isBot, isTrol
 
   const comments = await this.fetchComments(username)
 
-  return { ...user, comments, isBot, isTroll }
+  return { ...user, comments, is_bot: isBot, is_troll: isTroll }
 }
 
 /**

commit a23c565c57bd97f707a63c06d8a1e49150942c54

Fixes `isBot` name bug introduced in 854cbfa

kafka-export.js
@@ -60,6 +60,7 @@ function fetchProfiles (cursor, n, queue) {
          */
         (profile) => {
           console.info('kafka-export: processing profile', profile.data.name, 'with', profile.data.comments.length, 'comments')
+          // console.debug('kafka-export.js: profile.data', profile.data)
           const comments = profile.data.comments.reverse() // (`profile2` `comments` are expected in descending order.)
 
           let recentComments = [] // Start a data queue
@@ -70,8 +71,8 @@ function fetchProfiles (cursor, n, queue) {
             const fullComment = formatComment(profile.data, comment)
 
             // Sets is_bot and is_troll (coming originally from bots.csv).
-            fullComment.is_bot = profile.data.is_bot
-            fullComment.is_troll = profile.data.is_troll
+            fullComment.is_bot = profile.data.isBot
+            fullComment.is_troll = profile.data.isTroll
 
             // Attaches (≤20) recent comments by the same author PREVIOUS to this comment.
             fullComment.recent_comments = JSON.stringify(recentComments.slice(0, 20)) // JSON formatted string
@@ -145,6 +146,7 @@ async function main () {
        */
       async (comment, cb) => {
         // console.debug('kafka-export.js: kafkaQ sending JSON.stringify of', comment.link_id, 'to Kafka')
+        // console.debug('worker: comment data', comment)
         wait(500)
         // Try sending stringified JSON `comment` to Kafka (async fn) after 500 ms.
         if (NODE_ENV === 'production') {

kafka-stream.js
@@ -137,7 +137,8 @@ async function main () {
        * @returns {Promise<void>}
        */
       async (comment, cb) => {
-        // console.debug('worker: producing JSON.stringify of', comment.link_id, 'for Kafka')
+        // console.debug('worker: producing JSON.stringify of comment', comment.link_id, 'for Kafka')
+        // console.debug('worker: comment data', comment)
         wait(500)
         // Try sending stringified JSON `comment` to Kafka (async fn) after 500 ms.
         if (NODE_ENV === 'production') {