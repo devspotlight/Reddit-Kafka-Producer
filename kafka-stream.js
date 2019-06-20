95590acf0ebf2511a270f74b7ba0127cdacca46a
Jorge Orpinel <jorge@orpinel.com>
2019-06-20 00:52:09 -0500

    Fix rate limit pacing for Reddit API and Kafka producer, and
    Stop using dumb interval in favor of a recursive `stream` fn based on the Kafka producer queue saturation and draining.
    Partially WIP, see TODOs.

--- kafka-stream.js
+++ kafka-stream.js
@@ -27,81 +27,89 @@ let wait = ms => new Promise(resolve => setTimeout(resolve, ms))
 // Creates a ProfileScraper.
 const scraper = new ProfileScraper()
 
+let after = false
+
 /**
  * Fetches and process `n` comments from `subreddit`
  * @param subreddit to fetch
  * @param each callback to process the subreddit comment data
  */
-function fetchSubredditComments (subreddit, n, queue) {
+async function fetchSubredditComments (subreddit, n, queue) {
   // console.debug('worker: fetchSubredditComments fetching', n, 'comments from subreddit', subreddit)
 
   // Fetches reddit.com/${subreddit}/comments.json?limit=n
   let path = `https://www.reddit.com/r/${subreddit}/comments.json?limit=${n}`
 
-  // console.debug('worker: fetchSubredditComments requesting', path)
-  axios.get(path)
-    .then(
-      /**
-       * Async callback to process each comment in the 'politics' subreddit.
-       * Fetches comment author's profile data and formats the comment, before queueing it to be sent to Kafka.
-       * @param response from axios.get
-       */
-      (response) => {
-        // TODO: Use response.data.data.after for future call (`${path}&after=${after}`).
-        //       Requires re-engineering of interval so it waits for each axios.get call
+  let response
+  try {
+    console.debug('worker: fetchSubredditComments requesting', path)
+    response = await axios.get(path)
+    // /**
+    //  * Async callback to process each comment in the 'politics' subreddit.
+    //  * Fetches comment author's profile data and formats the comment, before queueing it to be sent to Kafka.
+    //  * @param response from axios.get
+    //  */
+    after = response.data.data.after
+    // TODO: Use `after` for next call (`${path}&after=${after}`)?
+    //       Requires re-engineering of interval so it waits for each axios.get call
+  } catch (ex) {
+    console.error('worker: fetch error!', ex)
+    return
+  }
+  // console.debug('worker: response', response)
 
-        console.info('worker: received', response.data.data.dist, 'comments from', subreddit)
+  console.info('worker: received', response.data.data.dist, 'comments from', subreddit)
 
-        // Processes each `response.data.data` (comment data) with given callback.
-        response.data.data.children.forEach(async (child) => {
-          const comment = child.data
-          const profile = await scraper.fetchProfile(comment.author)
+  console.debug(`worker: children`, response.data.data.children.map(
+    c => { return { link_id: c.data.link_id, created_utc: c.data.created_utc } }
+  ))
+  console.debug('worker: after', response.data.data.after)
 
-          if (profile.error) return
-          // TODO: Do anything else? (fetchProfile already logs an error.)
+  // Processes each `response.data.data` (comment data) with given callback.
+  response.data.data.children.forEach(async (child) => {
+    const comment = child.data
+    const profile = await scraper.fetchProfile(comment.author)
 
-          const fullComment = formatComment(profile, comment)
+    if (profile.error) return
+    // TODO: Do anything else? (fetchProfile already logs an error.)
 
-          // Sets is_bot and is_troll to `null` signaling we don't know yet.
-          fullComment.is_bot = null
-          fullComment.is_troll = null
+    const fullComment = formatComment(profile, comment)
 
-          // Fetch last ≤20 user comment Reddit ids to this comment (data structure).
-          let commentsAfterId = await scraper.fetchRecentComments(profile, comment.link_id, comment.created_utc, 20)
-          if (commentsAfterId.error) return
-          // TODO: Do anything else? (fetchRecentComments already logs an error.)
+    // Sets is_bot and is_troll to `null` signaling we don't know yet.
+    fullComment.is_bot = null
+    fullComment.is_troll = null
 
-          if (NODE_ENV !== 'production') {
-            console.info('worker: processing', comment.link_id, comment.created_utc, 'by', profile.name, '(with', commentsAfterId.length, 'recent commits)')
-          }
+    // Fetch last ≤20 user comment Reddit ids to this comment (data structure).
+    let commentsAfterId = await scraper.fetchRecentComments(profile, comment.link_id, comment.created_utc, 20)
+    if (commentsAfterId.error) return
+    // TODO: Do anything else? (fetchRecentComments already logs an error.)
 
-          // Attaches (≤20) previous comments by the same author to this comment (as a JSON formatted string).
-          fullComment.recent_comments = JSON.stringify(commentsAfterId)
+    if (NODE_ENV !== 'production') {
+      console.info('worker: processing', comment.link_id, comment.created_utc, 'by', profile.name, '(with', commentsAfterId.length, 'recent commits)')
+    }
 
-          // Marks record as NOT training data.
-          fullComment.is_training = false
+    // Attaches (≤20) previous comments by the same author to this comment (as a JSON formatted string).
+    fullComment.recent_comments = JSON.stringify(commentsAfterId)
 
-          // console.debug('worker: comment', comment)
-          // console.debug('worker: commentsAfterId', commentsAfterId)
-          // console.debug('worker: fullComment', fullComment)
+    // Marks record as NOT training data.
+    fullComment.is_training = false
 
-          // if (NODE_ENV !== 'production') {
-          //   // console.debug('worker: fetchSubredditComments cb sending', fullComment)
-          //   console.debug(
-          //     'worker: full comment has [ link_id, recent_comments ]',
-          //     [fullComment.link_id, commentsAfterId.map(c => { return { link_id: c.link_id, created_utc: c.created_utc } })]
-          //   )
-          // }
+    // console.debug('worker: comment', comment)
+    // console.debug('worker: commentsAfterId', commentsAfterId)
+    // console.debug('worker: fullComment', fullComment)
 
-          // Pushes `comment` task to `queue` (to be sent as message to Kafka).
-          console.debug('worker: pushing', fullComment.link_id, fullComment.created_utc)
-          queue.push(fullComment)
-        })
-      }
-    )
-    .catch((error) => {
-      console.error('worker: fetch error!', error)
-    })
+    // if (NODE_ENV !== 'production') {
+    //   // console.debug('worker: fetchSubredditComments cb sending', fullComment)
+    //   console.debug(
+    //     'worker: full comment has [ link_id, recent_comments ]',
+    //     [fullComment.link_id, commentsAfterId.map(c => { return { link_id: c.link_id, created_utc: c.created_utc } })]
+    //   )
+    // }
+
+    // Pushes `comment` task to `queue` (to be sent as message to Kafka).
+    console.debug('worker: pushing', fullComment.link_id, fullComment.created_utc)
+    queue.push(fullComment)
+  })
 }
 
 /**
@@ -143,7 +151,7 @@ async function main () {
       async (comment, cb) => {
         // console.debug('worker: producing JSON.stringify of comment', comment.link_id, 'for Kafka')
         // console.debug('worker: comment data', comment)
-        wait(500)
+
         // Try sending stringified JSON `comment` to Kafka (async fn) after 500 ms.
         if (NODE_ENV === 'production') {
           try {
@@ -162,35 +170,35 @@ async function main () {
         } else {
           console.info('worker: would produce (Kafka) message with key', `${comment.link_id}.${comment.created_utc} (comment by ${comment.author}). Q len`, kafkaQ.length())
         }
+        await wait(500) // pace Kafka producer
         cb()
       },
-      1) // NOTE: concurrency HAS to be 1 for `after` to work correctly.
+      1) // NOTE: concurrency HAS to be 1 for `&after` to work correctly.
 
-    let interval
 
     /**
      * Fn (to be ran at interval) for streaming comments as messages to the Kafka topic
      */
-    const stream = () => {
-      // If the queue is over 50 elements long, the interval stops.
-      if (kafkaQ.length() > 50) {
-        clearInterval(interval)
-        // console.debug('worker interval: suspending...')
+    const stream = async () => {
+      let n = 10 // comments to fetch per request
+      let m = 5 // (times `n` comments) to consider Kafka producer queue saturated
 
-        // But re-starts the interval when the last item from queue `kafkaQ` has returned from its worker.
-        kafkaQ.drain = () => { // See https://caolan.github.io/async/docs.html#QueueObject
-          // console.debug('worker interval: restarting.')
-          interval = setInterval(stream, 1000)
-        }
-      } else {
-        // Gets 10 comments from 'politics' subreddit and queues each for processing.
-        // TODO: We may have already fetched some of these latest 10 comments 1 sec ago.
-        fetchSubredditComments('politics', 10, kafkaQ)
+      // Processes and queues `n` comments.
+      await fetchSubredditComments('politics', n, kafkaQ)
+
+      await wait(5000) // pace Reddit API requests
+
+      // If Kafka producer queue isn't saturated, fetch more comments.
+      if (kafkaQ.length() < n * m) {
+        stream()
       }
+
+      // TODO: This should happen before the Kafka producer queue clears for efficiency.
+      // When Kafka producer queue clears, start over
+      kafkaQ.drain = stream
     }
 
-    // Starts the 1s interval.
-    interval = setInterval(stream, 1000)
+    stream()
   } catch (error) {
     console.error('worker error!', error)
   }
